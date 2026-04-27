82b50e30be9ab6c00bbed3eeee0fe8f1a888ddf4
abdiraimov-otabek <otabekabdiraimovv@gmail.com>
2026-04-27 15:42:00 +0500

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
 import express from "express";
+import cors from "cors";
+
+import router from "./routes/subjects";
 
 const app = express();
 const PORT = 3000;
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", router);
 
 app.get("/", (req, res) => {
   res.send("Hello, world!");
